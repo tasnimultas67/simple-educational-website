6a5438b57b681549e21c76906e756e47a27e7129
https://github.com/tasnimultas67 <tasnimultas67@gmail.com>
2021-10-04 23:57:47 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Components/Header/NavBar/Home/Home';
+import About from './Components/Header/NavBar/About/About';
+import NotFound from './Components/Header/NavBar/NotFound/NotFound';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Contact from './Components/Header/NavBar/Contact/Contact';
+import NavLinks from './Components/Header/NavBar/NavLinks/NavLinks';
+import Courses from './Components/Courses/Courses';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <div>
+        <Router>
+          <div className='nav-links'>
+            <NavLinks></NavLinks>
+          </div>
+        <Switch>
+          <Route exact path='/'>
+            <Home></Home>
+          </Route>
+
+          <Route path='/home'>
+            <Home></Home>
+          </Route>
+
+          <Route path='/courses'>
+            <Courses></Courses>
+          </Route>
+
+          <Route path='/about'>
+            <About></About>
+          </Route>
+
+          <Route path='/contact'>
+            <Contact></Contact>
+          </Route>
+
+          <Route path='*'>
+            <NotFound></NotFound>
+          </Route>
+
+        </Switch>
+        
+        <Footer></Footer>
+      </Router>
+
+        </div>
+      </div>
     </div>
   );
 }
